51b598e27e927371d04b029f6a9d8d57a4c3df11
hammanb <37665345+hammanb@users.noreply.github.com>
2021-12-12 17:08:00 -0800

    Update GottaCatchemAll.js

--- problem3/GottaCatchemAll.js
+++ problem3/GottaCatchemAll.js
@@ -17,7 +17,8 @@ class Parent extends GrandParent {
       if (badsad) {
         //if the idea is to keep the grandparent async, then parent also needs to be async, and the async grandparent needs to be called with await. 
         //child is not expecting anything returned, and neither is the initial call at line 35, so no promise needs to be created. 
-        //I am not entirely comfortable with my understanding of async / await with inheritance, and having trouble finding a docs that confirm my understanding in this case. 
+        //I am not entirely comfortable with my understanding of async / await with inheritance, and having trouble finding a docs that confirm my understanding
+        //The code executes as I expect it to. Please let me know if there was a more "correct" answer. 
         await this.kaboom()
       }
     } catch(err) {
